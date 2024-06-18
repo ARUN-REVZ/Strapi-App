8427be937e0ed6e1591d9a0eb9134f1a88f25d34
Albatroz <118113700+ARUNKUMAAR-R@users.noreply.github.com>
2024-06-18 15:03:24 +0530

    Update Strapi_on_Ubuntu.sh

--- Strapi_on_Ubuntu.sh
+++ Strapi_on_Ubuntu.sh
@@ -2,10 +2,7 @@
 sudo apt update
 curl -fsSL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
 bash -E nodesource_setup.sh
-node -v && npm -v
-sudo npm install -g yarn
-npm create-strapi-app simple-strapi --quickstart
-
-cd simple-strapi
-yarn develop
+sudo apt install nodejs -y
+npm install -g yarn
+echo -e "skip\n" | npx create-strapi-app simple-strapi --quickstart
 
